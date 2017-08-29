c85f63f9b545e3dea7873c1cdce52a1fdcda8e5a
Alejandro Morales <Alejandro Morales@DESKTOP-497A7L9>
2017-08-29 11:35:04 -0500

    Commit Idomas

---
+++ AppForeignExchange/AppForeignExchange/Interfaces/ILocalize.cs
@@ -0,0 +1,10 @@
+﻿namespace AppForeignExchange.Interfaces
+{
+    using System.Globalization;
+
+    public interface ILocalize
+    {
+        CultureInfo GetCurrentCultureInfo();
+        void SetLocale(CultureInfo ci);
+    }
+}
